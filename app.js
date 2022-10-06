e6126a0c23e3d03119e66968bdebdbab58c6a3ff
unknown <kimjh0069@gmail.com>
2022-10-06 16:01:35 +0900

    edit editpage

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const userRoutes = require("./routes/users");
 const campgroundRoutes = require("./routes/campground");
 const reviewRoutes = require("./routes/reviews");
 
-const dbUrl = process.env.DB_URL;
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
 // mongodb connection
 // "mongodb://localhost:27017/yelp-camp" - local connection
 // process.env.DB_URL - mongo atlas
